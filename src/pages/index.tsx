65e9e28c827b1681d0ccc0a28b2e50aae7e12f96
Karl Dreher <karldreher@outlook.com>
2023-03-30 20:20:11 -0700

    fix: remove dead import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import ActionAreaCard from '@/components/card'
 
 const inter = Inter({ subsets: ['latin'] })
 
