09ca3c7a2d42dd4c2dc329adc32dab4318c71bd9
Karl Dreher <karldreher@outlook.com>
2023-08-06 20:19:44 -0700

    feat: another set

--- src/resources/cards.js
+++ src/resources/cards.js
@@ -1,5 +1,15 @@
 const decks = [
     {
+        "title": "xmen92",
+        "description": "Impel 1992",
+        "cards": 100,
+        "root": "https://uncannyxmen.net/sites/default/files/images/cards/xmen92/",
+        // The number of digits for card numbers below 10.
+        "lpad": 2
+        //todo: special cards
+    },
+
+    {
         "title": "xmen93",
         "description": "Skybox 1993",
         "cards": 100,
