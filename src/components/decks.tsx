d191cdee7908d10ea69f3a2efc26ebafd23dd92a
Karl Dreher <karldreher@outlook.com>
2023-08-13 20:52:37 -0700

    fix: cleanup css

--- src/components/decks.tsx
+++ src/components/decks.tsx
@@ -1,24 +1,27 @@
 import Link from 'next/link';
 import * as React from 'react';
+import styles from '../styles/custom.module.css'
 import decks from '../resources/cards.js'
 
 export default function DeckList() {
     const links = decks.map((element) => {
         const randomStartCard = Math.floor(Math.random() * element.cards)
         return (
-            <li key={element.title}>
-                <h2>
-                    <Link href={`/cards/${element.title}/${randomStartCard}`}>{element.title}</Link>
-                </h2>
-                <div className='description'>{element.description}</div>
-                <div className='description'>{element.cards} cards</div>
-            </li>)
+                <li className={styles.deckslist} key={element.title}>
+                    <h2>
+                        <Link href={`/cards/${element.title}/${randomStartCard}`}>{element.title}</Link>
+                    </h2>
+                    <div className={styles.deckdescription}>manufacturer: {element.manufacturer}</div>
+                    <div className={styles.deckdescription}>year: {element.year}</div>
+                    <div className={styles.deckdescription}>cards: {element.cards}</div>
+                </li>
+        )
     })
     return (
         <div>
-			<p>Click a link below to explore the cards in that set.</p>
+            <h2>Click a link below to explore the cards in that set.</h2>
             <br></br>
-            <ul>{links}</ul>
+            <ul className={styles.ul}>{links}</ul>
         </div>
 
     )
--- src/resources/cards.js
+++ src/resources/cards.js
@@ -1,7 +1,8 @@
 const decks = [
     {
         "title": "xmen92",
-        "description": "Impel 1992",
+        "manufacturer": "Impel",
+        "year": 1992,
         "cards": 100,
         "root": "https://uncannyxmen.net/sites/default/files/images/cards/xmen92/",
         // The number of digits for card numbers below 10.
@@ -11,7 +12,8 @@ const decks = [
 
     {
         "title": "xmen93",
-        "description": "Skybox 1993",
+        "manufacturer": "Skybox",
+        "year": 1993,
         "cards": 100,
         "root": "https://uncannyxmen.net/sites/default/files/images/cards/xmen93/",
         // The number of digits for card numbers below 10.
