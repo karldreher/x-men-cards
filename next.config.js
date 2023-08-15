3d99a349cefbeca01c65171ac13a026a504944f3
Karl Dreher <karldreher@outlook.com>
2023-08-14 20:41:36 -0700

    feat: utilize next image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uncannyxmen.net',
+        port: '',
+        pathname: '/sites/**',
+      },
+    ],
+  },
   async headers() {
     return [
         {
