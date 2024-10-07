6a067d1c0372ed032f7b1c77dc305816e27429bb
Karl Dreher <karldreher@outlook.com>
2024-10-06 19:50:14 -0700

    feat: update navbutton

--- src/components/navbutton.tsx
+++ src/components/navbutton.tsx
@@ -1,7 +1,18 @@
 import { useRouter } from 'next/router'
 import { Button } from '@mui/material';
 
-function NavButton(props: any) {
+type NavButtonProps = {
+    /**
+     * The direction of the button, for the label.
+     */
+    direction: string
+    /**
+     * The number of zeros to pad the image number with.
+     */
+    lpad: number
+}
+
+function NavButton(props: NavButtonProps) {
     const router = useRouter()
     const splitPath = router.asPath.split('/')
     const set = splitPath[splitPath.length - 2]
@@ -11,15 +22,12 @@ function NavButton(props: any) {
     // TODO If next/prev is in omit list, choose next/prev again
     const prevImage = (String(Number(image) - 1)).padStart(props.lpad, "0")
     const nextImage = (String(Number(image) + 1)).padStart(props.lpad, "0")
-    if (props.direction == "next") {
-        var href = `/cards/${set}/${nextImage}`
-    }
-    if (props.direction == "prev") {
-        var href = `/cards/${set}/${prevImage}`
-    }
-    const handleClick = (e: any) => {
-        e.preventDefault()
-        router.push(href)
+    const cardIndex = props.direction === "next" ? nextImage : prevImage
+    // Assemble the link target, based on the direction.
+    const linkTarget = `/cards/${set}/${cardIndex}`
+    // Handle the button click event.
+    const handleClick = () => {
+        router.push(linkTarget)
     }
     return (
         <Button onClick={handleClick}>{props.direction}</Button>
